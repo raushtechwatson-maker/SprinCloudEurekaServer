c05a7eaf11cac87404afe3d6e33614c49872cf57
Raushan Kumar <raushtechwatson@gmail.com>
2026-06-14 00:31:26 +0530

    spring cloud Service Discovery concept using eureka server

---
+++ src/main/java/com/ServiceDiscoveryProject/ServiceDiscoveryProjectApplication.java
@@ -0,0 +1,16 @@
+package com.ServiceDiscoveryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryProjectApplication.class, args);
+	}
+
+}
